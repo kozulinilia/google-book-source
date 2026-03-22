b6d2dd12e793cf6fbb7e9bebe8e2f32162c197b4
Agapov Andrey <110200029+AgapAnd@users.noreply.github.com>
2026-03-22 14:05:18 +0300

    BS-04 feat: added clearing of order description  fields before its opening;#BS-04 feat: added buttons for showing new or sent orders;#BS-04 feat: added book's author to order description,#BS-04 feat: added viewer for orders on Order page,#BS-04 feat: renamed orders page in list of pages (#11)

--- Environment.js
+++ Environment.js
@@ -2,7 +2,7 @@ function loadEnvironment() {
     GLOBAL.tableId = PropertiesService.getScriptProperties().getProperty('TABLE_ID')
     GLOBAL.booksSheet = PropertiesService.getScriptProperties().getProperty('BOOKS_SHEET')
     GLOBAL.themesSheet = PropertiesService.getScriptProperties().getProperty('THEME_SHEET')
-    GLOBAL.offersSheet = PropertiesService.getScriptProperties().getProperty('OFFERS_SHEET')
+    GLOBAL.ordersSheet = PropertiesService.getScriptProperties().getProperty('ORDERS_SHEET')
     GLOBAL.recipientsSheet = PropertiesService.getScriptProperties().getProperty('RECIPIENTS_SHEET')
     GLOBAL.imgDir = PropertiesService.getScriptProperties().getProperty('BOOK_IMAGE_DIR')
     GLOBAL.imgTrashDir = PropertiesService.getScriptProperties().getProperty('BOOK_TRASH_DIR')
